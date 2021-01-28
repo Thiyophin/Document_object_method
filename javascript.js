16625068a2f8a085d42c8aa893dcb00bec398f38
Thiyophin <thiyophinj99@gmail.com>
2021-01-28 17:10:50 +0530

    dom initial commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+// form submit event
+form.addEventListener('submit', addItems);
+  //event listener for delete Item
+itemList.addEventListener('click',removeItem);
+// Add item
+function addItems(e){
+    e.preventDefault();
+   // Get input value
+ var newItem = document.getElementById('item').value;
+ //create new li element
+ var li = document.createElement('li');
+ //Add class for li
+ li.className = 'list-group-item';
+ // Add text node with input value
+// console.log(li);
+ li.appendChild(document.createTextNode(newItem));
+// create a delete button for new li
+var deleteBtn = document.createElement('button');
+// add class for deleteBtn
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+//create text for deleteBtn
+deleteBtn.appendChild(document.createTextNode('X'));
+// append deleteBtn to li
+li.appendChild(deleteBtn);
+ itemList.appendChild(li);
+}
+
+ //function to delete list
+ function removeItem(e){
+   //check delete class present in it
+  if(e.target.classList.contains('delete')){
+    // confirm alert
+    if(confirm('Are you sure?')){
+    var li = e.target.parentElement;
+    itemList.removeChild(li);
+    }
+  }
+}
